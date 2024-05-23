dc0b6d8f66ff9b0cd2a2315a0e0e1673a74fd728
nicolasscolnik <nicolasscolnik@gmail.com>
2024-05-23 20:11:11 -0300

    instalacion firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:cine_practica/core/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
